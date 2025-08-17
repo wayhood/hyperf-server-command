f208b8c6c0c021f02964a8f44ac9da77ea0581a2
wayhood <wayhood@163.com>
2025-08-18 06:06:18 +0800

    fix

--- src/Server/StopServer.php
+++ src/Server/StopServer.php
@@ -10,6 +10,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Hyperf\Command\Annotation\Command as HyperfCommand;
 use Psr\Container\ContainerInterface;
+use Hyperf\Contract\ConfigInterface;
 
 #[HyperfCommand]
 class StopServer extends Command
